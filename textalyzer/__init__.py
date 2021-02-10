ec045589f34d105260203d0cef452f65a56b0e31
Ruben Peters <rubenpeters91@protonmail.com>
2021-02-10 13:31:02 +0100

    Initial flask structure and basic css

---
+++ textalyzer/__init__.py
@@ -0,0 +1,22 @@
+from flask import (Flask, render_template)
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/summarizer')
+def summarizer():
+    return render_template('summarizer.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
