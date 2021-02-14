2c49a7da2333020c1a39ea9fe72176a3740c45b8
Ruben Peters <rubenpeters91@protonmail.com>
2021-02-14 14:26:27 +0100

    simplify Javascript function

--- textalyzer/static/summarise.js
+++ textalyzer/static/summarise.js
@@ -1,23 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+function make_summary() {
     let summary_form = document.forms['summaryform']
 
-    summary_form.addEventListener('submit', (event) => {
-        let summary = summary_form['originaltext'].value;
-        let sent_length = summary_form['sentlength'].value
+    let summary = summary_form['originaltext'].value;
+    let sent_length = summary_form['sentlength'].value
 
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                json_response = JSON.parse(this.response)
-                summary_form['summarizedtext'].innerHTML = json_response['summary'];
-                summary_form['inputlength'].value = json_response['input_length'];
-            }
-        };
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            json_response = JSON.parse(this.response)
+            summary_form['summarizedtext'].value = json_response['summary'];
+            summary_form['inputlength'].value = json_response['input_length'];
+        }
+    };
 
-        form_data = { 'originaltext': summary, 'sentlength': sent_length }
-        xhttp.open('POST', '/make_summary', true);
-        xhttp.setRequestHeader('Content-type', 'application/json');
-        xhttp.send(JSON.stringify(form_data));
-        event.preventDefault();
-    });
-})
+    form_data = { 'originaltext': summary, 'sentlength': sent_length }
+    xhttp.open('POST', '/make_summary', true);
+    xhttp.setRequestHeader('Content-type', 'application/json');
+    xhttp.send(JSON.stringify(form_data));
+}
