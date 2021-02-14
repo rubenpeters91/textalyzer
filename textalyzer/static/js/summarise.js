81bf3d83dffcbe76f7c693c74b045f3dcb697562
Ruben Peters <rubenpeters91@protonmail.com>
2021-02-14 17:40:48 +0100

    Added spinner and dutch as language

--- textalyzer/static/js/summarise.js
+++ textalyzer/static/js/summarise.js
@@ -3,17 +3,25 @@ function make_summary() {
 
     let summary = summary_form['originaltext'].value;
     let sent_length = summary_form['sentlength'].value
+    let language = summary_form['setlanguage'].value
 
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            json_response = JSON.parse(this.response)
-            summary_form['summarizedtext'].value = json_response['summary'];
-            summary_form['inputlength'].value = json_response['input_length'];
+        if (this.readyState < 4) {
+            document.getElementById('summary-spinner').style.display = ''
+        } else {
+            document.getElementById('summary-spinner').style.display = 'none'
+            if (this.readyState == 4 && this.status == 200) {
+                json_response = JSON.parse(this.response)
+                summary_form['summarizedtext'].value = json_response['summary'];
+                summary_form['inputlength'].value = json_response['input_length'];
+            } else {
+                summary_form['summarizedtext'].value = 'Something went wrong...';
+            }
         }
     };
 
-    form_data = { 'originaltext': summary, 'sentlength': sent_length }
+    form_data = { 'originaltext': summary, 'sentlength': sent_length, 'language': language }
     xhttp.open('POST', '/make_summary', true);
     xhttp.setRequestHeader('Content-type', 'application/json');
     xhttp.send(JSON.stringify(form_data));
