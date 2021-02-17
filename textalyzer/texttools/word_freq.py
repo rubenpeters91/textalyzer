e1302634528f62c9b5ef9d30575b06e087624d16
Ruben Peters <rubenpeters91@protonmail.com>
2021-02-17 16:21:40 +0100

    Fix templates and add docs

--- textalyzer/texttools/word_freq.py
+++ textalyzer/texttools/word_freq.py
@@ -5,9 +5,30 @@ import numpy as np
 
 class WordFreq(TextTool):
     def __init__(self, language: str = 'en'):
+        """Word frequency
+        Preprocesses the text and then uses spacy filters to determine
+        the term frequency and plot the result.
+
+        Parameters
+        ----------
+        language: str (default: "en")
+            Which languagemodel to use, only English ("en")
+            and Dutch ("nl") are supported at the moment
+        """
         super().__init__(language)
 
-    def plot_wordfreq(self, max_terms: int = 10):
+    def plot_wordfreq(self, max_terms: int = 10) -> Figure:
+        """Plot keyword frequency
+
+        Parameters
+        ----------
+        max_terms: int (default: 10)
+            The maximum number of terms to plot
+
+        Returns
+        -------
+        A Matplotlib Figure with the max frequency terms
+        """
         word_array = np.array(list(self.freq_word.keys()))
         value_array = np.array(list(self.freq_word.values()))
 
