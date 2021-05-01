01bc6c2136275511dba4b021659fe5eea5b092e9
Ruben Peters <rubenpeters91@protonmail.com>
2021-05-01 12:06:40 +0200

    Added Dockerfile and fixed dependencies

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='textalyzer',
-    version='0.4.0',
+    version='0.5.0',
     author='Ruben Peters',
     url='https://github.com/rubenpeters91/textalyzer',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'gunicorn',
         'spacy',
         'numpy',
         'matplotlib',
+        'wordcloud',
         'wikipedia'
     ],
     entry_points={
